f1bd83379a62cf1f99c2256398631d807431839f
saraoriola <saraoriola@outlook.com>
2023-07-13 12:25:56 +0200

    end

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -44,24 +44,49 @@ const ProductController = {
     // Define an asynchronous function 'getProductsByName' to retrieve products by name
     async getProductsByName(req, res) {
         try {
-            // Check if the length of the search query exceeds 20 characters // VALIDATION
+            // Check if the length of the search query exceeds 20 characters
             if (req.params.name.length > 20) {
                 return res.status(400).send('Search query is too long');
             }
 
-            // Create a regular expression with the parameter 'name' received in the request
-            const name = new RegExp(req.params.name, "i"); // i for case-insensitive matching
-      
             // Find products that match the 'name' using the Product model
-            const products = await Product.find({ name });
-      
+            const products = await Product.find({ name: { $regex: req.params.name, $options: 'i' } });
+
             // Send the retrieved products in the response
             res.send(products);
         } catch (error) {
             console.log(error);
         }
+    },
+
+    // Define an asynchronous function 'delete' to delete a product by its ID
+    async delete(req, res) {
+        try {
+            // Use the 'Product' model to find and delete the product by its ID
+            const product = await Product.findByIdAndDelete(req.params._id);
+            // Send a response with a success message and the deleted product
+            res.send({ message: 'Product deleted', product });
+        } catch (error) {
+            console.error(error);
+            // Send an error response if there's a problem deleting the product
+            res.status(500).send({ message: 'There was a problem trying to remove the product' });
+        }
+    },
+
+    // Define an asynchronous function 'update' to update a product by its ID
+    update: async (req, res) => {
+        try {
+            // Use the 'Product' model to find and update the product by its ID
+            // Setting new: true stores the updated product in the product constant.
+            const product = await Product.findByIdAndUpdate(req.params._id, req.body, { new: true }); 
+            // Send a response with a success message and the updated product
+            res.send({ message: 'Product successfully updated', product });
+        } catch (error) {
+            console.error(error);
+            // Send an error response if there's a problem updating the product
+            res.status(500).send({ message: 'There was a problem trying to update the product' });
+        }
     }
-    
 };
 
 // Export the 'ProductController' object to be used in other parts of the application
--- routes/products.js
+++ routes/products.js
@@ -5,21 +5,25 @@ const router = express.Router();
 // Import the 'ProductController' module
 const ProductController = require('../controllers/ProductController');
 
-//CREATE
+// CREATE
 // Define a POST route for creating a product
 router.post('/', ProductController.create);
 
-//READ
+// READ
 // Define a GET route for retrieving all products
 router.get('/', ProductController.getAll);
 // Define a GET route for retrieving a product by ID
-router.get('/id/:_id', ProductController.getById);
+router.get('/id/:_id', ProductController.getById); //_id -> because it is named _id in the controller
+
 // Define a GET route for retrieving products by name
 router.get('/name/:name', ProductController.getProductsByName);
 
-//UPDATE
+// UPDATE
+router.put('/id/:_id', ProductController.update);
 
-//DELETE
+// DELETE
+// Define a DELETE route for deleting a product by ID
+router.delete('/id/:_id', ProductController.delete); //_id -> because it is named _id in the controller
 
 // Export the router to be used in other parts of the application
 module.exports = router;
