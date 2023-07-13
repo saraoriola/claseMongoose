170eb653a081731ac64a478f7a49b81b35473ff8
saraoriola <saraoriola@outlook.com>
2023-07-13 12:02:09 +0200

    act

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -16,6 +16,30 @@ const ProductController = {
             res.status(500).send({ message: 'There was an issue creating the product' });
         }
     },
+
+    // Define an asynchronous function 'getAll' to retrieve all products
+    async getAll(req, res) {
+        try {
+            // Use the 'Product' model to retrieve all products
+            const products = await Product.find();
+            // Send a response with the retrieved products
+            res.send(products);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    
+    // Define an asynchronous function 'getById' to retrieve a product by its ID
+    async getById(req, res) {
+        try {
+            // Use the 'Product' model to retrieve a product by its ID
+            const product = await Product.findById(req.params._id);
+            // Send a response with the retrieved product
+            res.send(product);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 };
 
 // Export the 'ProductController' object to be used in other parts of the application
--- routes/products.js
+++ routes/products.js
@@ -8,5 +8,11 @@ const ProductController = require('../controllers/ProductController');
 // Define a POST route for creating a product
 router.post('/', ProductController.create);
 
+// Define a GET route for retrieving all products
+router.get('/', ProductController.getAll);
+
+// Define a GET route for retrieving a product by ID
+router.get('/id/:_id', ProductController.getById); // _id -> because it is named _id in the controller
+
 // Export the router to be used in other parts of the application
 module.exports = router;
